53e2c0db4b94936f979b0adf1f705374d2a74bb2
Sergey <mors195@yahoo.com>
2025-10-13 23:05:22 +0300

    Scroll Complete

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {ChatComponent} from './chat/chat-component/chat-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ChatComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
